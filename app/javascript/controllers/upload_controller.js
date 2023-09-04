4e6f780435bb5b44802e90891d02f5e6952f34ed
William Feron <william_feron@hotmail.fr>
2023-09-04 18:00:43 +0800

    loading things

--- app/javascript/controllers/upload_controller.js
+++ app/javascript/controllers/upload_controller.js
@@ -2,13 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="upload"
 export default class extends Controller {
-  static targets = ["button"];
+  static targets = ["button", "loader"];
 
   connect() {
     console.log(this.buttonTarget)
   }
 
   changed() {
+    this.buttonTarget.classList.toggle("d-none");
+  }
+
+  clicked() {
+    console.log(this.loaderTarget);
+    this.loaderTarget.classList.toggle("d-none");
     this.buttonTarget.classList.toggle("d-none");
   }
 }
