b2675b414a8adef8b14a63c86c5da9782430c162
Christian Morris <cjmorris96@hotmail.com>
2023-09-06 17:15:54 +0800

    promt improvements & loading story injection

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -122,7 +122,7 @@ class DishesController < ApplicationController
       resp = OpenaiChatgpt::Client.new(api_key: ENV["OPENAI_API_KEY"]).completions(
         model: "gpt-3.5-turbo",
         messages: [
-        { role: "user", content: "find all of the meals and separate them from the text in an array of hashes, with their respective descriptions: #{filtered_json_response},
+        { role: "user", content: "Find all of the meals and separate them from the given text in an array of hashes, with their respective descriptions (only return the array, nothing else): #{filtered_json_response},
             format: 'json'" }
         ]
       )
--- app/javascript/controllers/upload_controller.js
+++ app/javascript/controllers/upload_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="upload"
 export default class extends Controller {
-  static targets = ["button", "loader"];
+  static targets = ["button", "loader", "message"];
 
   connect() {
     console.log(this.buttonTarget)
@@ -16,5 +16,16 @@ export default class extends Controller {
     console.log(this.loaderTarget);
     this.loaderTarget.classList.toggle("d-none");
     this.buttonTarget.classList.toggle("d-none");
+    console.log(this.messageTarget);
+
+    setTimeout(() => {this.messageTarget.innerHTML = "<p class='message-style'>-->Your menu has been stolen by AI!<--</p>"}, 10000);
+    setTimeout(() => {this.messageTarget.innerHTML = "<p class='message-style'>-->Our Burger-Boi is fighting the AI 🤜😈<--</p>"}, 15000);
+    setTimeout(() => {this.messageTarget.innerHTML = "<p class='message-style'>-->Oooh, we found the Icecream 🤤🍦<--</p>"}, 20000);
+    setTimeout(() => {this.messageTarget.innerHTML = "<p class='message-style'>-->...We're almost there! I can taste it...<--</p>"}, 25000);
+    setTimeout(() => {this.messageTarget.innerHTML = "<p class='message-style'>-->Damn! This is one big menu?!<--</p>"}, 30000);
+    setTimeout(() => {this.messageTarget.innerHTML = "<p class='message-style'>-->K.O. Burger-Boi vs AI 😵<--</p>"}, 35000);
+    setTimeout(() => {this.messageTarget.innerHTML = "<p class='message-style'>-->Okay... We're experiencing a few difficulties...<--</p>"}, 40000);
+    setTimeout(() => {this.messageTarget.innerHTML = "<p class='message-style'>-->Eurika! We've sorted that out. Almost there 😁<--</p>"}, 45000);
+
   }
 }
