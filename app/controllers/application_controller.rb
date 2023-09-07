06c7542a562812f340a0c9c01605c88e510c6c8d
Mark Hording <markhording@gmail.com>
2023-09-07 16:28:53 +0800

    button and animation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
+  # rescue_from Exception, with: :handle_error_method
+  # rescue_from NameError, with: :handle_error_method
+
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+  def handle_error_method
+    flash[:notice] = "Burger Boi lost this round! 😢"
+    redirect_to root_path
   end
 end
