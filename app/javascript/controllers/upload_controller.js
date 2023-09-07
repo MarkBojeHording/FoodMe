898536729d0edfe65f38a766def4bb4bb0eac5c5
Mark Hording <markhording@gmail.com>
2023-09-07 17:11:53 +0800

    working-now

--- app/javascript/controllers/upload_controller.js
+++ app/javascript/controllers/upload_controller.js
@@ -18,14 +18,14 @@ export default class extends Controller {
     this.buttonTarget.classList.toggle("d-none");
     console.log(this.messageTarget);
 
-    setTimeout(() => {this.messageTarget.innerHTML = "<p class='message-style fade-in'>Your menu has been stolen by AI!</p>"}, 10000);
-    setTimeout(() => {this.messageTarget.innerHTML = "<p class='message-style fade-in'>Our Burger-Boi is fighting the AI 🤜😈</p>"}, 15000);
-    setTimeout(() => {this.messageTarget.innerHTML = "<p class='message-style fade-in'>Oooh, we found the Icecream 🤤🍦</p>"}, 20000);
-    setTimeout(() => {this.messageTarget.innerHTML = "<p class='message-style fade-in'>...We're almost there! I can taste it...</p>"}, 25000);
-    setTimeout(() => {this.messageTarget.innerHTML = "<p class='message-style fade-in'>Damn! This is one big menu?!</p>"}, 30000);
-    setTimeout(() => {this.messageTarget.innerHTML = "<p class='message-style fade-in'>K.O. Burger-Boi vs AI 😵</p>"}, 35000);
-    setTimeout(() => {this.messageTarget.innerHTML = "<p class='message-style fade-in'>Okay... We're experiencing a few difficulties...</p>"}, 40000);
-    setTimeout(() => {this.messageTarget.innerHTML = "<p class='message-style fade-in'>Eurika! We've sorted that out. Almost there 😁</p>"}, 45000);
+    setTimeout(() => {this.messageTarget.innerHTML = "<p data-notification-subscription-target='message' class='message-style fade-in'>Your menu has been stolen by AI!</p>"}, 10000);
+    setTimeout(() => {this.messageTarget.innerHTML = "<p data-notification-subscription-target='message' class='message-style fade-in'>Our Burger-Boi is fighting the AI 🤜😈</p>"}, 15000);
+    setTimeout(() => {this.messageTarget.innerHTML = "<p data-notification-subscription-target='message' class='message-style fade-in'>Oooh, we found the Icecream 🤤🍦</p>"}, 20000);
+    setTimeout(() => {this.messageTarget.innerHTML = "<p data-notification-subscription-target='message' class='message-style fade-in'>...We're almost there! I can taste it...</p>"}, 25000);
+    setTimeout(() => {this.messageTarget.innerHTML = "<p data-notification-subscription-target='message' class='message-style fade-in'>Damn! This is one big menu?!</p>"}, 30000);
+    setTimeout(() => {this.messageTarget.innerHTML = "<p data-notification-subscription-target='message' class='message-style fade-in'>K.O. Burger-Boi vs AI 😵</p>"}, 35000);
+    setTimeout(() => {this.messageTarget.innerHTML = "<p data-notification-subscription-target='message' class='message-style fade-in'>Okay... We're experiencing a few difficulties...</p>"}, 40000);
+    setTimeout(() => {this.messageTarget.innerHTML = "<p data-notification-subscription-target='message' class='message-style fade-in'>Eurika! We've sorted that out. Almost there 😁</p>"}, 45000);
 
   }
 }
